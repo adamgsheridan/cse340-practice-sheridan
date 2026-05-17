a4a5013674ca1d67408a4f7d7db34b6f9f4755a4
adamgsheridan <adamgsheridan7@gmail.com>
2026-05-16 22:27:10 -0400

    code challenge done

---
+++ src/controllers/faculty/faculty.js
@@ -0,0 +1,32 @@
+// Import faculty model functions
+import { getFacultyById, getSortedFaculty} from '../../models/faculty/faculty.js';
+
+// Function that renders the faculty list page
+const facultyListPage = (req, res) => {
+    const sortBy = req.query.sort || 'name';
+    const facultyList = getSortedFaculty(sortBy);
+    res.render('faculty/list', {
+        title: 'Faculty Directory',
+        faculty: facultyList,
+        currentSort: sortBy
+    });
+};
+
+// Function that uses route parameters to look up individual faculty.
+const facultyDetailPage = (req, res, next) => {
+    const facultyId = req.params.facultyId;
+    const facultyMember = getFacultyById(facultyId);
+    
+    // If faculty member doesn't exist, create 404 error
+    if (!facultyMember) {
+        const err = new Error(`Faculty member ${facultyId} not found`);
+        err.status = 404;
+        return next(err);
+    }
+    res.render('faculty/detail', {
+        title: facultyMember.name,
+        faculty: facultyMember
+    });
+};
+
+export { facultyListPage, facultyDetailPage };
